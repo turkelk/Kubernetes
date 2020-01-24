2acdaa7d33992d44ed969ee1f0a0e8cecfa6e0d2
Turkel Gadirzade <turkelgadirzade@Turkels-MacBook-Pro.local>
2020-01-24 17:54:49 +0400

    mysql deploy sh fix

--- MySQL/deploy.sh
+++ MySQL/deploy.sh
@@ -1,5 +1,6 @@
 kubectl create -f namespace.yaml 
-
 kubectl create secret generic mysql-root-pass --from-literal=password=pwd --namespace=db
 kubectl create secret generic mysql-user-pass --from-literal=username=tk --from-literal=password=pwd --namespace=db
 kubectl create secret generic mysql-db-url --from-literal=database=bex --from-literal=url='Server=mysql;Port=3306;Database=bex;Uid=root;tk=pwd;' --namespace=db
+kubectl apply -f pvc.yaml 
+kubectl apply -f deployment.yaml 
