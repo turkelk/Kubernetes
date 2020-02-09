9acf01bcefb4ead7477e89fc1470a2109d90a0b6
Turkel Gadirzade <turkelgadirzade@Turkels-MacBook-Pro.local>
2020-02-09 18:10:00 +0400

    masstransit added

--- Redis/Cluster/deploy.sh
+++ Redis/Cluster/deploy.sh
@@ -1,5 +1,5 @@
 kubectl create -f namespace.yaml 
-kubectl create secret generic redis-passwd --from-literal=passwd=${RANDOM} --namespace=redis
+# kubectl create secret generic redis-passwd --from-literal=passwd=1234 --namespace=redis
 kubectl create configmap redis-config --from-file=launch.sh=launch.sh --namespace=redis
 kubectl apply -f service.yaml 
 kubectl apply -f statefulset.yaml 
--- Redis/Cluster/launch.sh
+++ Redis/Cluster/launch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-PASSWORD=$( cat /etc/redis-passwd/passwd )
-if [[ "${HOSTNAME}" == "redis-0" ]] ; then 
-    redis-server --requirepass ${PASSWORD}
+# PASSWORD=$( cat /etc/redis-passwd/passwd )
+if [[ "${HOSTNAME}" != "redis-0" ]] ; then 
+    # redis-server --requirepass ${PASSWORD}
+    echo "ok"
 else 
-    redis-server --slaveof redis-0.redis 6379 --masterauth ${PASSWORD} --requirepass ${PASSWORD} 
+    redis-server --slaveof redis-0.redis 6379 
 fi
